5e473a14ab61428b3c7a11cf4d76d955443dabe0
Weiliang Xing <xingwl0401@gmail.com>
2015-06-03 10:57:58 -0400

    halfway for activation-error hold: account_activation_controller.rb with associate files, not account_activations_controller.rb as tutorial guides. route resources changed and also get the correct results.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,11 +19,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      #Handle a successful save.
-      flash[:success] = "Welcome to the Sample App!"
-      # redirect_to user_url(@user)
-      redirect_to @user
+      # log_in @user
+      # #Handle a successful save.
+      # flash[:success] = "Welcome to the Sample App!"
+      # # redirect_to user_url(@user)
+      # redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
